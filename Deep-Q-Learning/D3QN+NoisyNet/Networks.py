50b0d7244df1ba1088836905d89ba333c4cefab1
Ali Ebrazeh <88633795+AEbrazeh@users.noreply.github.com>
2024-06-06 23:29:47 +0330

    Add DQN+NoisyNet

---
+++ Deep-Q-Learning/D3QN+NoisyNet/Networks.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from torch.nn.functional import linear
+
+def scaledNoise(size):
+    noise = torch.randn(size)
+    return noise.abs().sqrt() * noise.sign()
+class noisyLinear(nn.Module):
+    def __init__(self, inputDim, outputDim, stdInit=0.5):
+        super(noisyLinear, self).__init__()
+        
+        self.inputDim = inputDim
+        self.outputDim = outputDim
+        self.stdInit = stdInit
+        
+        self.meanWeight = nn.Parameter(torch.Tensor(outputDim, inputDim))
+        self.meanWeight.data.uniform_(-1 / np.sqrt(max(inputDim, outputDim)), 1 / np.sqrt(max(inputDim, outputDim)))
+        
+        self.meanBias = nn.Parameter(torch.Tensor(outputDim))
+        self.meanBias.data.uniform_(-1 / np.sqrt(max(inputDim, outputDim)), 1 / np.sqrt(max(inputDim, outputDim)))
+        
+        self.stdWeight = nn.Parameter(torch.Tensor(outputDim, inputDim))
+        self.stdWeight.data.fill_(stdInit / np.sqrt(max(inputDim, outputDim)))
+        
+        self.stdBias = nn.Parameter(torch.Tensor(outputDim))
+        self.stdBias.data.fill_(stdInit / np.sqrt(max(inputDim, outputDim)))
+        
+        self.register_buffer('epsWeight', torch.Tensor(outputDim, inputDim))
+        self.register_buffer('epsBias', torch.Tensor(outputDim))
+        self.resetNoise()
+        
+    def resetNoise(self):
+        self.epsWeight.copy_(scaledNoise(self.outputDim).outer(scaledNoise(self.inputDim)))
+        self.epsBias.copy_(scaledNoise(self.outputDim))
+        
+    def forward(self, x):
+        return linear(x, self.meanWeight + self.epsWeight * self.stdWeight, self.meanBias + self.epsBias * self.stdBias)
+        
+class noisyNet(nn.Module):
+    def __init__(self, layersDim, lastLayerActivation = False):
+        """
+        Initializes a simple feed forward neural network.
+
+        Parameters:
+            layersDim (list): The dimensions of the layers.
+        Returns:
+            None
+        """
+        super(noisyNet, self).__init__()
+        self.network = nn.Sequential()
+        for ii in range(len(layersDim) - 2):
+            self.network.append(noisyLinear(layersDim[ii], layersDim[ii+1]))
+            self.network.append(nn.LeakyReLU(0.1))
+        self.network.append(noisyLinear(layersDim[-2], layersDim[-1]))
+        if lastLayerActivation: self.network.append(nn.LeakyReLU(0.1))
+        
+    def forward(self, x):
+        return self.network(x)
+    
+    def resetNoise(self):
+        for layer in self.network:
+            if isinstance(layer, noisyLinear): layer.resetNoise()
+    
+class qNetworkDuelling(nn.Module):
+    def __init__(self, stateDim, actionDim, hiddenDim, numHiddenLayers):
+        """
+        Initialize the Duelling Q-network with the given state dimension, hidden dimension, and number of hidden layers.
+        """
+        super(qNetworkDuelling, self).__init__()
+        numHL1 = numHiddenLayers // 2
+        numHL2 = numHiddenLayers - numHL1 + 1
+        self.baseNetwork = noisyNet([stateDim] + [hiddenDim for _ in range(numHL1)], lastLayerActivation=True)
+        self.advNetwork = noisyNet([hiddenDim for _ in range(numHL2)] + [actionDim])
+        self.valueNetwork = noisyNet([hiddenDim for _ in range(numHL2)] + [1])
+
+    def forward(self, state):
+        H = self.baseNetwork(state)
+        V = self.valueNetwork(H)
+        Adv = self.advNetwork(H)
+        return V + Adv - Adv.mean(-1, keepdim=True)
+    
+    def resetNoise(self):
+        self.baseNetwork.resetNoise()
+        self.valueNetwork.resetNoise()
+        self.advNetwork.resetNoise()
+        
+    def save(self, file):
+        torch.save(self, file)
---
+++ Deep-Q-Learning/D3QN+NoisyNet/Train.py
@@ -0,0 +1,46 @@
+from Agent import *
+import gymnasium as gym
+import matplotlib.pyplot as plt
+
+env = gym.make('CartPole-v1')#, render_mode="human")
+stateDim = env.observation_space.shape[0]
+actionDim = env.action_space.n
+agent = deepQLearningDoubleDuellingNoisy(stateDim, actionDim, hiddenDim=32, numHiddenLayers=2, lr=1e-3, tau=0.1)
+print(agent.critic)
+
+numEpisodes = 500
+batchSize = 512
+minibatchSize = 64
+numMiniBatch = np.ceil(batchSize/minibatchSize).astype(int)
+
+rewardHist = np.zeros(numEpisodes)
+
+numSteps = 0
+bestScore = -np.inf
+for ii in range(numEpisodes):
+    s_, _ = env.reset()
+    d = False
+    jj = 0
+    while not d:
+        numSteps += 1
+        s = s_
+        a = agent(s)
+        s_, r, terminated, truncated, _ = env.step(a)
+        d = max(truncated, terminated)
+        agent.store(s, a, r, d, s_)
+        rewardHist[ii] += r
+        jj += 1
+        if numSteps >= batchSize:
+            agent.learn(minibatchSize, numMiniBatch)
+        else:
+            agent.critic.resetNoise()
+        print("----------[Episode: {:04}/{:04}, Step: {:04}, Reward: {:.3f}, Best Reward: {:04.3f} ]----------".format(ii+1, numEpisodes, jj, rewardHist[ii], bestScore), end='\r', flush=True)
+    if rewardHist[ii].sum(-1) >= bestScore:
+        agent.save('best')
+        bestScore = rewardHist[ii].sum(-1)
+    print("----------[Episode: {:04}/{:04}, Step: {:04}, Reward: {:.3f}, Best Reward: {:.3f} ]----------".format(ii+1, numEpisodes, jj, rewardHist[ii], bestScore))
+    agent.save('last')
+env.close()
+    
+np.savetxt('rewardHist.csv', rewardHist)
+env.close()
